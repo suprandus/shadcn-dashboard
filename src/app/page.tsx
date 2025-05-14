e275e896fd29876c0546698b7eb52fdc55313ab3
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2025-05-14 22:53:01 +0800

    feat: add initial bar and area charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      {/* Call AppBarChart component */}
+      <div className="p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="p-4 rounded-lg">Test</div>
+      <div className="p-4 rounded-lg">Test</div>
+      <div className="p-4 rounded-lg">Test</div>
+      <div className="p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>{" "}
+      <div className="p-4 rounded-lg">Test</div>
+    </div>
+  );
 };
 
 export default Homepage;
